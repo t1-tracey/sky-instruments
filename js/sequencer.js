ba4923ab6c03313e5f3e621124edb986da2b137f
Tracey <traceyle@outlook.com>
2018-11-02 19:18:51 +1100

    Disable keyboard support
    making mobile not working

---
+++ js/sequencer.js
@@ -0,0 +1,7 @@
+function sequencer() {
+  //create a synth and connect it to the master output (your speakers)
+  var synth = new Tone.Synth().toMaster()
+
+  //play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease('C4', '8n')
+}
